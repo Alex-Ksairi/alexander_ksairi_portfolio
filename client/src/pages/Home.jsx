ec4a44f2e782341fa5adf0b73d0d75b5c41cb81d
Alex-Ksairi <alexanderksairi@web.de>
2022-02-06 13:19:38 +0100

    client: reset sass styles, colors

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+// importing page sections
+import About from "./About";
+import Skills from "./Skills";
+import Qualifications from "./Qualifications";
+import Projects from "./Projects";
+
 function Home() {
   return <div>
       <h1>Hello from home</h1>
+      <About />
+      <Skills />
+      <Qualifications />
+      <Projects />
   </div>;
 }
 
